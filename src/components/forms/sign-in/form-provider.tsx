add36185fd79b8e21386fb1a27ec934719ee5d75
dha201 <davidmha2001@gmail.com>
2024-06-27 17:34:24 -0400

    + Form component

--- src/components/forms/sign-in/form-provider.tsx
+++ src/components/forms/sign-in/form-provider.tsx
@@ -26,10 +26,22 @@ type Props = {
     children: React.ReactNode
 }
 
-const SignInFormProvider = (props: Props) => {
-  return (
-    <div>form-provider</div>
-  )
+const SignInFormProvider = ({ children }: Props) => {
+    const { methods, onHandleSubmit, loading } = useSignInForm()
+  
+    return (
+      <AuthContextProvider>
+        <FormProvider {...methods}>
+          <form onSubmit={onHandleSubmit} className="h-full">
+            <div className="flex flex-col justify-between gap-3 h-full">
+              <Loader loading={loading}>
+                {children}
+              </Loader>
+            </div>
+          </form>
+        </FormProvider>
+      </AuthContextProvider>
+    )
 }
 
 export default SignInFormProvider
